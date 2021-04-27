f2ddf09e92071135677795f9cef668a822c5b58a
André Teixeira <andremarcondesteixeira@gmail.com>
2021-04-27 20:51:34 -0300

    feature: implements endpoint using hardcoded data

--- src/main/java/ml/meajudadev/despesas/DespesasApplication.java
+++ src/main/java/ml/meajudadev/despesas/DespesasApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -19,12 +18,22 @@ public class DespesasApplication {
 	}
 
 	public DespesasApplication() {
-		categories = new ArrayList<ExpenseCategory>();
+		categories = List.of(
+				new ExpenseCategory(1, "Alimentação", true),
+				new ExpenseCategory(2, "Saúde", true),
+				new ExpenseCategory(3, "Entretenimento", true),
+				new ExpenseCategory(4, "Educação", true)
+		);
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/api/v1/expense-categories")
+	public List<ExpenseCategory> listCategories() {
+		return categories;
 	}
 }
 //{
--- src/main/java/ml/meajudadev/despesas/ExpenseCategory.java
+++ src/main/java/ml/meajudadev/despesas/ExpenseCategory.java
@@ -1,7 +1,9 @@
 package ml.meajudadev.despesas;
 
-public class ExpenseCategory {
-//        "id": 1,
-//        "description": "Alimentação",
-//        "enabled": true
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record ExpenseCategory(
+        @JsonProperty int id,
+        @JsonProperty String description,
+        @JsonProperty boolean enabled
+) { }
