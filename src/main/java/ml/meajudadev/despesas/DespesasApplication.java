abdc12551397d4d18b50a2af7f83ef881459e760
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-04-27 20:26:17 -0300

    feature: criação de Api categorias
    WIP

--- src/main/java/ml/meajudadev/despesas/DespesasApplication.java
+++ src/main/java/ml/meajudadev/despesas/DespesasApplication.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class DespesasApplication {
+	private List<ExpenseCategory> categories;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DespesasApplication.class, args);
+	}
+
+	public DespesasApplication() {
+		categories = new ArrayList<ExpenseCategory>();
 	}
 
 	@GetMapping("/hello")
@@ -19,3 +27,22 @@ public class DespesasApplication {
 		return String.format("Hello %s!", name);
 	}
 }
+//{
+//    "categories": [{
+//        "id": 1,
+//        "description": "Alimentação",
+//        "enabled": true
+//    }, {
+//        "id": 2,
+//        "description": "Saúde",
+//        "enabled": true
+//    }, {
+//        "id": 3,
+//        "description": "Entretenimento",
+//        "enabled": true
+//    }, {
+//        "id": 4,
+//        "description": "Educação",
+//        "enabled": true
+//    }]
+//}
---
+++ src/main/java/ml/meajudadev/despesas/ExpenseCategory.java
@@ -0,0 +1,7 @@
+package ml.meajudadev.despesas;
+
+public class ExpenseCategory {
+//        "id": 1,
+//        "description": "Alimentação",
+//        "enabled": true
+}
