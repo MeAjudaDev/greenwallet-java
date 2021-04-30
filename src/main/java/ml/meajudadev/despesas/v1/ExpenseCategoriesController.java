c96995c6fdb995d8be727593983025c34963c78b
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-04-29 22:46:12 -0300

    feature: make field categories mutable

--- src/main/java/ml/meajudadev/despesas/v1/ExpenseCategoriesController.java
+++ src/main/java/ml/meajudadev/despesas/v1/ExpenseCategoriesController.java
@@ -4,6 +4,8 @@ import ml.meajudadev.despesas.ExpenseCategory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -11,12 +13,14 @@ public class ExpenseCategoriesController {
     private List<ExpenseCategory> categories;
 
     public ExpenseCategoriesController() {
-        categories = List.of(
-                new ExpenseCategory(1, 1, "Alimentação", 'A', 'E'),
-                new ExpenseCategory(2, 1, "Saúde", 'A', 'E'),
-                new ExpenseCategory(3, 1, "Entretenimento", 'A', 'E'),
-                new ExpenseCategory(4, 1, "Educação", 'A', 'E')
-        );
+        categories = new ArrayList<ExpenseCategory>() {
+            {
+                add(new ExpenseCategory(1, 1, "Alimentação", 'A', 'E'));
+                add(new ExpenseCategory(2, 1, "Saúde", 'A', 'E'));
+                add(new ExpenseCategory(3, 1, "Entretenimento", 'A', 'E'));
+                add(new ExpenseCategory(4, 1, "Educação", 'A', 'E'));
+            }
+        };
     }
 
     @GetMapping("/api/v1/expense-categories")
