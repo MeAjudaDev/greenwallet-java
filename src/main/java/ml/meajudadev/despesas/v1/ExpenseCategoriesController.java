e71c90cad799166a80cc8b9acd095d562a8de982
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-04-29 23:22:51 -0300

    feature: create new endpoint for deleting expense categories

--- src/main/java/ml/meajudadev/despesas/v1/ExpenseCategoriesController.java
+++ src/main/java/ml/meajudadev/despesas/v1/ExpenseCategoriesController.java
@@ -1,12 +1,17 @@
 package ml.meajudadev.despesas.v1;
 
 import ml.meajudadev.despesas.ExpenseCategory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ExpenseCategoriesController {
@@ -26,5 +31,18 @@ public class ExpenseCategoriesController {
     @GetMapping("/api/v1/expense-categories")
     public List<ExpenseCategory> listCategories() {
         return categories;
+    }
+
+    @DeleteMapping("/api/v1/expense-categories/{id}")
+    public void deleteCategory(@PathVariable int id) {
+        var filter = categories.stream().filter(category -> {
+            return category.id() == id;
+        }).collect(Collectors.toList());
+
+        if(filter.size()==0){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        categories.remove(filter.get(0));
     }
 }
