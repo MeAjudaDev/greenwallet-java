dc78ef69190b3c744bd34e93c5ce21e22db780d4
Iago Rodrigues <yagomello16@gmail.com>
2021-05-06 23:31:40 -0300

    feat(Controller): adicionar ExpenseController

--- src/main/java/ml/meajudadev/despesas/v1/controllers/ExpensesController.java
+++ src/main/java/ml/meajudadev/despesas/v1/controllers/ExpensesController.java
@@ -1,4 +1,28 @@
 package ml.meajudadev.despesas.v1.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ml.meajudadev.despesas.Expense;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/expenses")
 public class ExpensesController {
+
+    private final List<Expense> expenses = new ArrayList<>();
+
+    public ExpensesController() {
+        expenses.add(new Expense(1, "Água", 73.21, true, "2021-04-08", Arrays.asList(1,5,3), 1));
+        expenses.add(new Expense(2, "Luz", 83.00, true, "2021-04-08", Arrays.asList(5,3),1));
+    }
+
+    @GetMapping
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
 }
