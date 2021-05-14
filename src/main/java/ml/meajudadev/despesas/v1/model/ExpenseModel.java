730a152c48c0645b5306d843c263bd0c8a8376ea
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-05-13 21:11:56 -0300

    refactor: unificação do ExpenseController

--- src/main/java/ml/meajudadev/despesas/v1/model/ExpenseModel.java
+++ src/main/java/ml/meajudadev/despesas/v1/model/ExpenseModel.java
@@ -3,16 +3,17 @@ package ml.meajudadev.despesas.v1.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Calendar;
 
 public record ExpenseModel(
         @JsonProperty("id") int id,
         @JsonProperty("userId") int userId,
-        @JsonProperty("categoryId") int categoryId,
+        @JsonProperty("categoryId") Integer categoryId,
         @JsonProperty("description") String description,
         @JsonProperty("value") BigDecimal value,
         @JsonProperty("isFixed") boolean isFixed,
-        @JsonProperty("dueDate") Calendar dueDate,
+        @JsonProperty("dueDate") LocalDate dueDate,
         @JsonProperty("expenseType") char expenseType,
         @JsonProperty("expenseState") char expenseState
 ) {
