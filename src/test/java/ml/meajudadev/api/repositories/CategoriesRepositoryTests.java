82519a5786fdda895a3f62bddf4d7e0e9a26b3df
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-06-08 20:52:28 -0300

    feat:criada CategoriesRepositoryTests

---
+++ src/test/java/ml/meajudadev/api/repositories/CategoriesRepositoryTests.java
@@ -0,0 +1,12 @@
+package ml.meajudadev.api.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CategoriesRepositoryTests {
+    @Test
+    public void testSetup() {
+        assertTrue(true);
+    }
+}
