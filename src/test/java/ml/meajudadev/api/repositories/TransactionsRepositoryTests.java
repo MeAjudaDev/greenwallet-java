fce95b7cb2b4daa08e16e2f193618ccf4a529457
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-08 21:16:14 -0300

    test(TransactionsRepositoryTests): asserting values

--- src/test/java/ml/meajudadev/api/repositories/TransactionsRepositoryTests.java
+++ src/test/java/ml/meajudadev/api/repositories/TransactionsRepositoryTests.java
@@ -2,11 +2,17 @@ package ml.meajudadev.api.repositories;
 
 import ml.meajudadev.api.entities.Transaction;
 import ml.meajudadev.api.entities.enums.TransactionState;
+import ml.meajudadev.api.entities.enums.TransactionType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -19,8 +25,26 @@ public class TransactionsRepositoryTests {
     public void givenNewTransaction_thenItCanBeSaved() {
         Transaction transaction = new Transaction();
         transaction.setCategoryId(1L);
+        transaction.setUserId(1L);
         transaction.setDescription("Curso de JavaScript");
         transaction.setFixed(false);
         transaction.setState(TransactionState.ACTIVE);
+        transaction.setType(TransactionType.EXPENSE);
+        transaction.setValue(50D);
+        transaction.setDueDate(LocalDate.of(2021, Month.JULY, 10));
+
+        repository.save(transaction);
+
+        assertEquals(3L, transaction.getId());
+        assertEquals(1L, transaction.getCategoryId());
+        assertEquals(1L, transaction.getUserId());
+        assertEquals("Curso de JavaScript", transaction.getDescription());
+        assertFalse(transaction.isFixed());
+        assertEquals(TransactionState.ACTIVE, transaction.getState());
+        assertEquals(TransactionType.EXPENSE, transaction.getType());
+        assertEquals(50D, transaction.getValue());
+        assertEquals(LocalDate.of(2021, Month.JULY, 10), transaction.getDueDate());
+        assertNotNull(transaction.getCreatedAt());
+        assertNotNull(transaction.getUpdateAt());
     }
 }
