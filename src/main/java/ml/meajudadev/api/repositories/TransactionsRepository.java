3483cc7d5de5b6f0a45aed4397928f53195328d3
Thaís Barbosa <thaismb26@gmail.com>
2021-07-13 20:34:50 -0300

    test(data.sql): fix syntax error

--- src/main/java/ml/meajudadev/api/repositories/TransactionsRepository.java
+++ src/main/java/ml/meajudadev/api/repositories/TransactionsRepository.java
@@ -1,5 +1,6 @@
 package ml.meajudadev.api.repositories;
 
+import ml.meajudadev.api.entities.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Repository;
 public class TransactionsRepository {
     @Autowired
     JdbcTemplate db;
+
+    public void save(Transaction transaction) {
+
+    }
 }
