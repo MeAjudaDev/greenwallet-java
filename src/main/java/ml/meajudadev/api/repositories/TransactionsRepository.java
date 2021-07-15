8bd62749d30bb068f29d3aa0aad1b90c0effc81a
Thaís Barbosa <thaismb26@gmail.com>
2021-07-15 20:26:20 -0300

    feat(TransactionsRepository): add method getById

--- src/main/java/ml/meajudadev/api/repositories/TransactionsRepository.java
+++ src/main/java/ml/meajudadev/api/repositories/TransactionsRepository.java
@@ -1,6 +1,8 @@
 package ml.meajudadev.api.repositories;
 
 import ml.meajudadev.api.entities.Transaction;
+import ml.meajudadev.api.entities.enums.TransactionState;
+import ml.meajudadev.api.entities.enums.TransactionType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,5 +19,23 @@ public class TransactionsRepository {
         transaction.setId(3L);
         transaction.setCreatedAt(LocalDate.of(2021, Month.JULY, 13));
         transaction.setUpdatedAt(LocalDate.of(2021, Month.JULY, 13));
+    }
+
+    public Transaction getById(Long id) {
+        Transaction transaction = new Transaction();
+
+        transaction.setId(3L);
+        transaction.setCategoryId(1L);
+        transaction.setUserId(1L);
+        transaction.setDescription("Curso de JavaScript");
+        transaction.setFixed(false);
+        transaction.setState(TransactionState.ACTIVE);
+        transaction.setType(TransactionType.EXPENSE);
+        transaction.setValue(50D);
+        transaction.setDueDate(LocalDate.of(2021, Month.JULY, 10));
+        transaction.setCreatedAt(LocalDate.of(2021, Month.JULY, 13));
+        transaction.setUpdatedAt(LocalDate.of(2021, Month.JULY, 13));
+
+        return transaction;
     }
 }
