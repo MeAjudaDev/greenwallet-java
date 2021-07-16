8f0580a948904680c9ff883280d74b4a32d72b66
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-15 23:26:32 -0300

    chore(tests): updates sql files

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO users (name, email, password, activation_code, state) VALUES ('Geno 
 INSERT INTO categories (user_id, name, enabled, type) VALUES (1, 'Educação', 1, 'E');
 INSERT INTO categories (user_id, name, enabled, type) VALUES (1, 'Salário', 1, 'R');
 
-INSERT INTO transactions VALUES (null, 1, 1, 'Curso de Java na Udemy', 27.00, false, '2021-07-08', 'E', 'A', null, null);
-INSERT INTO transactions VALUES (null, 2, 2, 'Ganhos da empresa', 100.00, true, '2021-07-08', 'R', 'A', null, null);
+INSERT INTO transactions (user_id, category_id, description, value, is_fixed, due_date, type, state) VALUES (1, 1, 'Curso de Java na Udemy', 27.00, false, '2021-07-08', 'E', 'A');
+INSERT INTO transactions (user_id, category_id, description, value, is_fixed, due_date, type, state) VALUES (2, 2, 'Ganhos da empresa', 100.00, true, '2021-07-08', 'R', 'A');
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -5,33 +5,33 @@ create table users (
     password varchar(100) not null,
     activation_code varchar(4),
     state char default 'P',
-    update_at datetime on update current_timestamp,
-    created_at datetime default current_timestamp
+    created_at datetime default current_timestamp,
+    last_updated_at datetime on update current_timestamp
 );
 
 create table categories (
     id int AUTO_INCREMENT primary key,
-    user_id int not null,                           -- Referencia ao usuário que criou a categoria
-    name varchar(100) not null,                     -- Nome da Categoria ( Obrigatorio )
+    user_id int not null,                           -- Referência ao usuário que criou a categoria
+    name varchar(100) not null,                     -- Nome da categoria (obrigatorio)
     enabled boolean default 1 not null,             -- 1 = True = Enabled / 0 = False = Disabled
     type char not null,                             -- [E] Despesas / [R] Receitas
-    update_at datetime on update current_timestamp,
     created_at datetime default current_timestamp,
+    last_updated_at datetime on update current_timestamp,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 create table transactions (
     id int AUTO_INCREMENT primary key,
-    user_id int not null,                           -- Referencia ao usuário que criou a transação
-    category_id int not null,                       -- Referencia à categoria dessa transação
+    user_id int not null,                           -- Referência ao usuário que criou a transação
+    category_id int not null,                       -- Referência à categoria dessa transação
     description varchar(150) not null,              -- Descrição da transação
     value decimal(15, 2) not null,                  -- Valor da transação
     is_fixed boolean not null,
-    due_date date not null,                         -- Data de Vencimento ou Data de Recebimento
+    due_date date not null,                         -- Data de vencimento ou recebimento
     type char not null,                             -- Tipo da transação: [E] Expense (Despesas) / [R] Revenue (Receitas)
     state char default 'A',                         -- [A] Ativo / [D] Desativado (a) / [E] Excluido (a)
-    updated_at datetime on update current_timestamp,
     created_at datetime default current_timestamp,
+    last_updated_at datetime on update current_timestamp,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
