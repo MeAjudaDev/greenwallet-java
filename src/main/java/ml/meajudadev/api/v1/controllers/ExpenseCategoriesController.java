d2cc2fb7041263155b1623755f58ce0a207de418
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-20 20:11:44 -0300

    style(ExpenseCategoriesController): removes unused import

--- src/main/java/ml/meajudadev/api/v1/controllers/ExpenseCategoriesController.java
+++ src/main/java/ml/meajudadev/api/v1/controllers/ExpenseCategoriesController.java
@@ -4,7 +4,6 @@ import ml.meajudadev.api.repositories.CategoriesRepository;
 import ml.meajudadev.api.v1.dto.ExpenseCategoryDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
