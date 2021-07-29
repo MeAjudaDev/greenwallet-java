20148766008ac07a8b1b2e9ce6bdcb231235b8b2
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-29 20:50:27 -0300

    feat(ExpensesController): start using TransactionRepository

--- src/main/java/ml/meajudadev/api/v1/controllers/ExpensesController.java
+++ src/main/java/ml/meajudadev/api/v1/controllers/ExpensesController.java
@@ -1,6 +1,11 @@
 package ml.meajudadev.api.v1.controllers;
 
+import ml.meajudadev.api.entities.Transaction;
+import ml.meajudadev.api.entities.enums.TransactionState;
+import ml.meajudadev.api.entities.enums.TransactionType;
+import ml.meajudadev.api.repositories.TransactionsRepository;
 import ml.meajudadev.api.v1.dto.ExpenseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,36 +18,43 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/expenses")
 public class ExpensesController {
-    private final List<ExpenseDto> expenses;
+    @Autowired
+    TransactionsRepository repository;
 
-    public ExpensesController() {
-        expenses = new ArrayList<>();
-    }
+    private final List<ExpenseDto> expenses = new ArrayList<>();
 
     @GetMapping
     public List<ExpenseDto> getExpenses(
             @RequestParam(value = "start_date", required = false) LocalDate startDate,
             @RequestParam(value = "end_date", required = false) LocalDate endDate,
-            @RequestParam(value = "category", required = false) Integer category
+            @RequestParam(value = "category_id", required = false) Integer categoryId
     ) {
         return expenses
                 .stream()
-                .filter(expense -> category == null || expense.categoryId().equals(category))
+                .filter(expense -> categoryId == null || expense.categoryId() == categoryId)
                 .filter(expense -> startDate == null || expense.dueDate().isEqual(startDate) || expense.dueDate().isAfter(startDate))
                 .filter(expense -> endDate == null || expense.dueDate().isEqual(endDate) || expense.dueDate().isBefore(endDate))
                 .collect(Collectors.toList());
     }
 
     @PostMapping
-    public void newExpense(@RequestBody ExpenseDto expense) {
-        expenses.add(expense);
+    public void newExpense(@RequestBody ExpenseDto dto) {
+        var transaction = new Transaction()
+                .setCategoryId(dto.categoryId())
+                .setDescription(dto.description())
+                .setFixed(dto.isFixed())
+                .setDueDate(dto.dueDate())
+                .setState(TransactionState.of(dto.expenseState()))
+                .setType(TransactionType.of(dto.expenseType()))
+                .setUserId(dto.userId())
+                .setValue(dto.value())
+                .setCreatedAt(LocalDate.now());
+        repository.save(transaction);
     }
 
     @GetMapping("/{id}")
     public ExpenseDto getExpenseById(@PathVariable int id) {
-        List<ExpenseDto> result = expenses.stream().filter(expense -> {
-            return expense.id() == id;
-        }).collect(Collectors.toList());
+        List<ExpenseDto> result = expenses.stream().filter(expense -> expense.id() == id).collect(Collectors.toList());
 
         if (result.size() == 0) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
@@ -65,9 +77,7 @@ public class ExpensesController {
 
     @DeleteMapping("/{id}")
     public void deleteExpense(@PathVariable int id) {
-        List<ExpenseDto> result = expenses.stream().filter(expense -> {
-            return expense.id() == id;
-        }).collect(Collectors.toList());
+        List<ExpenseDto> result = expenses.stream().filter(expense -> expense.id() == id).collect(Collectors.toList());
 
         if (result.size() == 0) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
